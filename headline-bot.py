6286ff098ab5cc40f03cf16be2640bf5b66e95a1
Philipp Wachowitz <philipp.wachowitz@gmx.de>
2025-03-04 11:32:50 +0100

    first commit

---
+++ headline-bot.py
@@ -0,0 +1,15 @@
+# Gets the recent news headline from tageschau and appends it to a file
+import requests
+
+# do the api request
+def get_headline(index=0):
+    try:
+        return requests.get("https://www.tagesschau.de/api2u/news").json()["news"][index]["title"]
+    except IndexError:
+        return ("Index does not exist. Try a smaller number.")
+    
+# append current headline to file
+with open("headlines.txt", "a") as file:
+    file.write(get_headline() + "\n")
+
+print("Headline appended!")
