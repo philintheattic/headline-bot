195ec41b0319010ae4311bc78d9c747df143f97a
Philipp Wachowitz <philipp.wachowitz@gmx.de>
2025-03-13 20:55:36 +0100

    added option to output structured poems

--- sort-pos.py
+++ sort-pos.py
@@ -1,23 +1,34 @@
 import random
 
-with open("headlines.txt", "r", encoding="utf-8") as file:
-    nouns = []
-    rest = []
-    for line in file:
-        for word in line.split():
-            if word.istitle():
-                nouns.append(word)
-            else:
-                rest.append(word)
+nouns = []
+verbs = []
+adjectives = []
+sentence_structures = [
+    ["NOUN", "VERB", "ADJ", "NOUN"],
+    ["ADJ", "NOUN", "VERB"],
+    ["NOUN", "VERB", "ADJ"]
+]
 
-n = 3
-out = []
+word_pools = {"NOUN": nouns, "VERB": verbs, "ADJ": adjectives}
 
-nouns_sample = random.sample(nouns, n)
-verbs_sample = random.sample(rest, n)
+def fill_word_pool(nouns: list, verbs: list, adjectives: list):
+    with open("headlines.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            for word in line.split():
+                if word.istitle():
+                    nouns.append(word)
+                else:
+                    if word.endswith("en") or word.endswith("t"):
+                        verbs.append(word)
+                    else:
+                        adjectives.append(word)
 
-for noun, verb in zip(nouns_sample, verbs_sample):
-    out.append(f"{noun} {verb}")
-    
-output_text = " ".join(out)
-print(output_text)
+fill_word_pool(nouns, verbs, adjectives)
+
+def generate_sentence():
+    structure = random.choice(sentence_structures)
+    words = [random.choice(word_pools[pos]) for pos in structure]
+    return " ".join(words)
+
+for _ in range(3):
+    print(generate_sentence())
